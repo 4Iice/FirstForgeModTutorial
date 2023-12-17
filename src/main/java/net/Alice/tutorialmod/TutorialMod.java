9e42535368abe3cc92ba790969557687a7e015c6
Charlie <charlie.wegner.2004@gmail.com>
2023-12-17 20:40:24 +0100

    test command

--- src/main/java/net/Alice/tutorialmod/TutorialMod.java
+++ src/main/java/net/Alice/tutorialmod/TutorialMod.java
@@ -13,7 +13,7 @@ import org.slf4j.Logger;
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(TutorialMod.MOD_ID)
 public class TutorialMod {
-
+//test command
     public static final String MOD_ID = "tutorialmod";
 
     private static final Logger LOGGER = LogUtils.getLogger();
